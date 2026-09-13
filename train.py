8fabed259569c58209cbfa51d637635f95c37965
sjrom47 <sergiojimenezromero4@gmail.com>
2026-09-13 22:01:35 +0200

    feat: finished mlflow section

--- train.py
+++ train.py
@@ -6,6 +6,10 @@ from sklearn.metrics import accuracy_score
 import joblib
 import mlflow
 import mlflow.sklearn
+# added dagshub for recommended way to use mlflow
+import dagshub
+dagshub.init(repo_owner='sjrom47', repo_name='mlops-practica-icai', mlflow=True)
+
 # Cargar el conjunto de datos
 iris = datasets.load_iris()
 X = iris.data
@@ -14,20 +18,22 @@ y = iris.target
 with mlflow.start_run():
     # Dividir los datos en conjuntos de entrenamiento y prueba
     X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=42
+        X, y, test_size=0.3, random_state=42
     )
     # Inicializar y entrenar el modelo
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    n_estimators = 50
+    #! Cambiar el log de n_estimators
+    model = RandomForestClassifier(n_estimators=n_estimators, random_state=42)
     model.fit(X_train, y_train)
     # Realizar predicciones y calcular la precisión
     y_pred = model.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
     # Guardar el modelo entrenado en un archivo .pkl
-    joblib.dump(model, 'model.pkl')
+    joblib.dump(model, "model.pkl")
     # Registrar el modelo con MLflow
     mlflow.sklearn.log_model(model, "random-forest-model")
     # Registrar parámetros y métricas
-    mlflow.log_param("n_estimators", 100)
+    mlflow.log_param("n_estimators", n_estimators)
     mlflow.log_metric("accuracy", accuracy)
     print(f"Modelo entrenado y precisión: {accuracy:.4f}")
     print("Experimento registrado con MLflow.")
