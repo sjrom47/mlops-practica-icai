1b5c87ff073cff94b50dbb4819ef23b3c7e93dcd
sjrom47 <sergiojimenezromero4@gmail.com>
2026-09-25 09:35:04 +0200

    feat: added dvc part of the pipeline

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import pandas as pd
 from sklearn import datasets
@@ -10,58 +11,72 @@ import mlflow.sklearn
 import matplotlib.pyplot as plt
 import seaborn as sns
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
 tracking_uri = os.environ.get("MLFLOW_TRACKING_URI")
 mlflow.set_tracking_uri(tracking_uri)
 
-# Cargar el conjunto de datos desde el archivo CSV
-try:
-    iris = pd.read_csv("data/iris_dataset.csv")
-except FileNotFoundError:
-    print("Error: El archivo 'data/iris_dataset.csv' no fue encontrado.")
 
-# Dividir el DataFrame en características (X) y etiquetas (y)
-X = iris.drop("target", axis=1)
-y = iris["target"]
+def train_model(n_estimators):
+    # Cargar el conjunto de datos desde el archivo CSV
+    try:
+        iris = pd.read_csv("data/iris_dataset.csv")
+        X = iris.drop("target", axis=1)
+        y = iris["target"]
+    except FileNotFoundError:
+        print("Error: El archivo 'data/iris_dataset.csv' no fue encontrado.")
+        # Fallback: cargar el conjunto de datos de sklearn
+        iris = datasets.load_iris()
+        X = iris.data
+        y = iris.target
+
+    # Iniciar un experimento de MLflow
+    with mlflow.start_run():
+        # Dividir los datos en conjuntos de entrenamiento y prueba
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.3, random_state=42
+        )
+        # Inicializar y entrenar el modelo
+        model = RandomForestClassifier(n_estimators=n_estimators, random_state=42)
+        model.fit(X_train, y_train)
+        # Realizar predicciones y calcular la precisión
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        # Guardar el modelo entrenado en un archivo .pkl
+        joblib.dump(model, "model.pkl")
+        # Registrar el modelo con MLflow
+        mlflow.sklearn.log_model(model, "random-forest-model")
+        # Registrar parámetros y métricas
+        mlflow.log_param("n_estimators", n_estimators)
+        mlflow.log_metric("accuracy", accuracy)
+        print(f"Modelo entrenado y precisión: {accuracy:.4f}")
+        print("Experimento registrado con MLflow.")
+        # --- Sección de Reporte para CML ---
+        # 1. Generar la matriz de confusión
+        cm = confusion_matrix(y_test, y_pred)
+        plt.figure(figsize=(8, 6))
+        sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
+        plt.title("Matriz de Confusión")
+        plt.xlabel("Predicciones")
+        plt.ylabel("Valores Reales")
+        plt.savefig("confusion_matrix.png")
+        print("Matriz de confusión guardada como 'confusion_matrix.png'")
+        # --- Fin de la sección de Reporte ---
+        mlflow.log_artifact("confusion_matrix.png")
+        metrics = {"accuracy": accuracy}
+        with open("mlflow_metrics.json", "w") as f:
+            json.dump(metrics, f)
 
 
-# Cargar el conjunto de datos
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
-# Iniciar un experimento de MLflow
-with mlflow.start_run():
-    # Dividir los datos en conjuntos de entrenamiento y prueba
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, random_state=42
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--n_estimators",
+        type=int,
+        default=100,
+        help="Number of estimators for RandomForestClassifier",
     )
-    # Inicializar y entrenar el modelo
-    n_estimators = 50
-    #! Cambiar el log de n_estimators
-    model = RandomForestClassifier(n_estimators=n_estimators, random_state=42)
-    model.fit(X_train, y_train)
-    # Realizar predicciones y calcular la precisión
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    # Guardar el modelo entrenado en un archivo .pkl
-    joblib.dump(model, "model.pkl")
-    # Registrar el modelo con MLflow
-    mlflow.sklearn.log_model(model, "random-forest-model")
-    # Registrar parámetros y métricas
-    mlflow.log_param("n_estimators", n_estimators)
-    mlflow.log_metric("accuracy", accuracy)
-    print(f"Modelo entrenado y precisión: {accuracy:.4f}")
-    print("Experimento registrado con MLflow.")
-    # --- Sección de Reporte para CML ---
-    # 1. Generar la matriz de confusión
-    cm = confusion_matrix(y_test, y_pred)
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
-    plt.title("Matriz de Confusión")
-    plt.xlabel("Predicciones")
-    plt.ylabel("Valores Reales")
-    plt.savefig("confusion_matrix.png")
-    print("Matriz de confusión guardada como 'confusion_matrix.png'")
-    # --- Fin de la sección de Reporte ---
+    args = parser.parse_args()
+    train_model(args.n_estimators)
